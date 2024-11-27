c83ad7d271e13b95d57343f0c904e5c32455bc07
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:43:52 -0500

    Update script.js
    Added ability to generate playbook choice buttons and added click event listener (only changes background color for now)

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
 //HTML Declarations
 const BLOODLINECONTAINER = document.getElementById("bloodline-container");
+const ORIGINCONTAINER = document.getElementById("origin-container");
 const BLOODLINECHOICE = document.getElementById("bloodline-choice");
+const ORIGINCHOICE = document.getElementById("origin-choice");
 
-//Importing
-import { ardentPlaybook } from "./playbooks/bloodlines/ardent.js";
-import { ektusPlaybook } from "./playbooks/bloodlines/ektus.js";
+import { PLAYBOOKS } from "./importer.js";
 
-//Aggregation Variables
-const BLOODLINES = [ardentPlaybook, ektusPlaybook];
+//Playbook choice template
+const choiceBtnTemplate = (name, type, blurb) => {
+  return `<div id='${name}' class='${type.toLowerCase()}-choice-btn playbook-choice-btn'>
+      <h2 class='text-center'>
+      ${name}
+      </h2>
+      <p class='text-center'>
+      ${blurb}
+      </p></div>`;
+};
 
-//Load Content
-const loadContent = function () {
-  for (const bloodline of BLOODLINES) {
-    //console.log(bloodline.name);
-    BLOODLINECHOICE.innerHTML += `<div id=${bloodline.name}><p>${bloodline.name}</p></div>`;
+//Create playbook choice buttons
+const loadChoiceBtns = function () {
+  for (const playbook of PLAYBOOKS) {
+    if (playbook.type == "Bloodline") {
+      BLOODLINECHOICE.innerHTML += choiceBtnTemplate(
+        playbook.name,
+        playbook.type,
+        playbook.blurb
+      );
+    } else if (playbook.type == "Origin") {
+      ORIGINCHOICE.innerHTML += choiceBtnTemplate(
+        playbook.name,
+        playbook.type,
+        playbook.blurb
+      );
+    }
   }
 };
 
-window.onload = loadContent();
+//Call loadChoiceBtns before assigning button functionality
+loadChoiceBtns();
+
+//Create array of selectable buttons
+const playbookChoiceBtns = document.getElementsByClassName(
+  "playbook-choice-btn"
+);
+
+//Add function to buttons
+[...playbookChoiceBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    let clickedBtn = event.target;
+    if (
+      event.target.tagName.toLowerCase() == "p" ||
+      event.target.tagName.toLowerCase() == "h2"
+    ) {
+      clickedBtn = event.target.parentElement;
+    }
+
+    //Unselect a button of the same type as the clicked button
+    [...playbookChoiceBtns].forEach((item) => {
+      if (
+        item.classList.contains("selected-playbook") &&
+        item.classList.contains(clickedBtn.classList[0]) &&
+        item.id !== clickedBtn.id
+      ) {
+        item.classList.toggle("selected-playbook");
+      }
+    });
+
+    clickedBtn.classList.toggle("selected-playbook");
+  });
+});
