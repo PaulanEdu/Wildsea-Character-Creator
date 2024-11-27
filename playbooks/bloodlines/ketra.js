e921c00475130eba6e821b6efd921cdcbaf4ed9b
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:48:10 -0500

    Create ketra.js
    Bare bones for now

---
+++ playbooks/bloodlines/ketra.js
@@ -0,0 +1,18 @@
+export const ketraPlaybook = {
+  name: "Ketra",
+  type: "Bloodline",
+  blurb:
+    "Humanity's less fortunate descendants, making their way up from hidden mines and buried buildings. The ketra are gelatinous, tech-savvy, and bio-electric.",
+  edgesQS: [],
+  skillsQS: [],
+  languagesQS: [],
+  resourcesQS: {
+    salvage: [],
+    specimens: [],
+    whispers: [],
+    charts: [],
+  },
+  drivesQS: [],
+  miresQS: [],
+  aspects: [],
+};
