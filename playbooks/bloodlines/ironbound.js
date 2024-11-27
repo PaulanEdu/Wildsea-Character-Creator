c4534bcacd2f97a27be4dded37e087a0e5828134
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:47:53 -0500

    Create ironbound.js
    Bare bones for now

---
+++ playbooks/bloodlines/ironbound.js
@@ -0,0 +1,18 @@
+export const ironboundPlaybook = {
+  name: "Ironbound",
+  type: "Bloodline",
+  blurb:
+    "Ship-spirits with driftwood bodies, out to sail the waves once more. The Ironbound are naturally spiritual and sturdily built.",
+  edgesQS: [],
+  skillsQS: [],
+  languagesQS: [],
+  resourcesQS: {
+    salvage: [],
+    specimens: [],
+    whispers: [],
+    charts: [],
+  },
+  drivesQS: [],
+  miresQS: [],
+  aspects: [],
+};
