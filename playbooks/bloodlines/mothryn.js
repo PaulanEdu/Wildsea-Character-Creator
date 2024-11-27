037114fda670c54850fb6ec9993c3776fe4f715f
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:48:29 -0500

    Create mothryn.js
    Bare bones for now

---
+++ playbooks/bloodlines/mothryn.js
@@ -0,0 +1,18 @@
+export const mothrynPlaybook = {
+  name: "Mothryn",
+  type: "Bloodline",
+  blurb:
+    "An insectile bloodline exploring the leafy sea after the collapse of their restrictive pre-verdant culture. The mothryn are short-lived and colorful.",
+  edgesQS: [],
+  skillsQS: [],
+  languagesQS: [],
+  resourcesQS: {
+    salvage: [],
+    specimens: [],
+    whispers: [],
+    charts: [],
+  },
+  drivesQS: [],
+  miresQS: [],
+  aspects: [],
+};
