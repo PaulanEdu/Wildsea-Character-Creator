ef2335db8204c0b405b5e58f212b330920c894d2
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:44:56 -0500

    Update ardent.js
    Changed aspects to an array of objects, rather than an object of objects.

--- playbooks/bloodlines/ardent.js
+++ playbooks/bloodlines/ardent.js
@@ -20,115 +20,115 @@ export const ardentPlaybook = {
     "The Call of the Under-Eaves grows strong",
     "Your ancestors whisper of their dissappointment",
   ],
-  aspects: {
-    0: {
+  aspects: [
+    {
       name: "Tough as Nails",
       type: "Trait",
       length: 4,
       description:
         "You're a natural survivor. Rolls made to heal an injury you're suffering from treat conflicts as triumphs.",
     },
-    1: {
+    {
       name: "Ghostsight",
       type: "Trait",
       length: 3,
       description:
         "You can clearly see and easily communicate with spirits and the spectral realm, and can idenfiy spirit-anchored objects by sight.",
     },
-    2: {
+    {
       name: "Strong Stomach",
       type: "Trait",
       length: 3,
       description: "Reduce the impact of poisons, diseases, and sickness.",
     },
-    3: {
+    {
       name: "Ironclad Mind",
       type: "Trait",
       length: 4,
       description:
         "You are immune to hallucinations, mesmerics, and mental compulsions.",
     },
-    4: {
+    {
       name: "Guide My Hands",
       type: "Trait",
       length: 3,
       description:
         "Burn to borrow a measure of an ancestor's competency, gaining two ranks in any skill for the duration of the scene.",
     },
-    5: {
+    {
       name: "Sporescraf",
       type: "Gear",
       length: 4,
       description:
         "Thickly-woven fibres sealed with tar. Resist the effect of bad air and airborne spores.",
     },
-    6: {
+    {
       name: "Pre-Verdant Heirloom",
       type: "Gear",
       length: 3,
       description:
         "A vestige of the long-buried past. When you would mark mire you can choose to mark this instead.",
     },
-    7: {
+    {
       name: "Segmented Torc",
       type: "Gear",
       length: 2,
       description:
         "Crafted from reclaimed wreckage, said to hold the luck that others lack. Whenever you gain a temporary benefit, increase that benefit's track by one box.",
     },
-    8: {
+    {
       name: "Ajna",
       type: "Gear",
       length: 4,
       description:
         "A stringed instrument designed to be played by a duo of the living and the dead, one using a bow and one plucking with spectral fingers. Mark to allow all present to see and communicate with spirits and the spectral realm for the duration of a song.",
     },
-    9: {
+    {
       name: "Scimitar",
       type: "Gear",
       length: 4,
       description: "A curved blade that deals CQ Keen damage.",
     },
-    10: {
+    {
       name: "Machete",
       type: "Gear",
       length: 2,
       description:
         "A trusty blade used as a tool as often as it is a weapon. Deals CQ Hewing damage. Increase impact when clearing vegetation with this gear.",
     },
-    11: {
+    {
       name: "Hand Crossbow",
       type: "Gear",
       length: 4,
       description: "A compact crossbow. Dealers LR Spike damage.",
     },
-    12: {
+    {
       name: "Raged Longcoat",
       type: "Gear",
       length: 3,
       description:
         "You're resistant to three damage types, chosen from the following list: Blunt, Keen, Hewing, Serrated.",
     },
-    13: {
+    {
       name: "Ancestral Spirit",
       type: "Companion",
       length: 3,
       description:
         "You're haunted by a benevolent spirit. Mark to learn a secret from the spectral realm (once per scene).",
     },
-    14: {
+    {
       name: "Watch-Wolf",
       type: "Companion",
       length: 3,
       description:
         "A loyal lupine companion that will defend you to the death. Deals CQ Spike damage.",
     },
-    15: {
+    {
       name: "Grizzled Ratter",
       type: "Companion",
       length: 3,
       description:
         "A sly ship-born feline, adept at pursuing tiny creatures. Mark to create a resource, <em>Hunter's Offering</em>",
     },
-  },
+  ],
 };
