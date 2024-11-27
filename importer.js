d1d3d2fe4333f459b205c9e38837754a6a0546cb
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:46:37 -0500

    Create importer.js
    Currently importing all bloodlines and Amberclad

---
+++ importer.js
@@ -0,0 +1,20 @@
+//Importing
+import { ardentPlaybook } from "./playbooks/bloodlines/ardent.js";
+import { ektusPlaybook } from "./playbooks/bloodlines/ektus.js";
+import { gauPlaybook } from "./playbooks/bloodlines/gau.js";
+import { ironboundPlaybook } from "./playbooks/bloodlines/ironbound.js";
+import { ketraPlaybook } from "./playbooks/bloodlines/ketra.js";
+import { mothrynPlaybook } from "./playbooks/bloodlines/mothryn.js";
+import { tzelicraePlaybook } from "./playbooks/bloodlines/tzelicrae.js";
+import { ambercladPlaybook } from "./playbooks/origins/amberclad.js";
+
+export const PLAYBOOKS = [
+  ardentPlaybook,
+  ektusPlaybook,
+  gauPlaybook,
+  ironboundPlaybook,
+  ketraPlaybook,
+  mothrynPlaybook,
+  tzelicraePlaybook,
+  ambercladPlaybook,
+];
