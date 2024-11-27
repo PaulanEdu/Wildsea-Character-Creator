3958ec99caa308c28c57a55651b21628609db066
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:47:26 -0500

    Create gau.js
    Bare bones for now

---
+++ playbooks/bloodlines/gau.js
@@ -0,0 +1,18 @@
+export const gauPlaybook = {
+  name: "Gau",
+  type: "Bloodline",
+  blurb:
+    "Fungal humanoids set free from their old homes by this new world of rot and growth. The gau are swift and spry, making the best of their freedom.",
+  edgesQS: [],
+  skillsQS: [],
+  languagesQS: [],
+  resourcesQS: {
+    salvage: [],
+    specimens: [],
+    whispers: [],
+    charts: [],
+  },
+  drivesQS: [],
+  miresQS: [],
+  aspects: [],
+};
