b501339afa80cb61e809327993ae8c9424fdfed3
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-11-27 02:48:50 -0500

    Create tzelicrae.js
    Bare bones for now

---
+++ playbooks/bloodlines/tzelicrae.js
@@ -0,0 +1,18 @@
+export const tzelicraePlaybook = {
+  name: "Tzelicrae",
+  type: "Bloodline",
+  blurb:
+    "Secretive spider-colonies wearing humanesque skins. The tzelicrae are mysterious and unsettling, masters of movement and mimicry.",
+  edgesQS: [],
+  skillsQS: [],
+  languagesQS: [],
+  resourcesQS: {
+    salvage: [],
+    specimens: [],
+    whispers: [],
+    charts: [],
+  },
+  drivesQS: [],
+  miresQS: [],
+  aspects: [],
+};
